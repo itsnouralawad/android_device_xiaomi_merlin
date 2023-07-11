4a722ddda239b43ecad4224048c593b4bcefa290
itsnouralawad <itsnouralawad@gmail.com>
2023-07-11 23:23:12 +0000

    merlin: Import permission Config From Stock

--- device.mk
+++ device.mk
@@ -18,6 +18,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
+    
+#permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-platform.xml
 
 # Health
 PRODUCT_PACKAGES += \
