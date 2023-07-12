52a6f446d3892a4583f9761319c94c31d1b2b1c4
itsnouralawad <itsnouralawad@gmail.com>
2023-07-12 06:22:55 +0000

    merlin : Build CAS blobs from Source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,10 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_PREBUILT_ELF_FILES := true
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
+# CAS
+PRODUCT_PACKAGES += \
+    android.hardware.cas@1.2-service-lazy
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor:64 \
