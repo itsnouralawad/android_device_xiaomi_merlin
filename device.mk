662c1b8436241b8c0d46287d509735f47141af52
itsnouralawad <itsnouralawad@gmail.com>
2023-07-14 05:23:13 +0000

    merlin : Build Display Libs from source

--- device.mk
+++ device.mk
@@ -64,6 +64,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.1.vendor:64
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-resources.vendor:64 \
+    android.hardware.graphics.composer@2.1.vendor:64 \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-impl \
+    libdrm.vendor \
+    libvulkan
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor:64 \
