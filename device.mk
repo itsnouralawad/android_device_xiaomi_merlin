334f0ba12b36167b6ecff8ee50f4bb9f8fac2005
itsnouralawad <itsnouralawad@gmail.com>
2023-07-15 03:22:39 +0000

    merlin : Build USB Libs from source

--- device.mk
+++ device.mk
@@ -246,6 +246,15 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0.vendor \
+    android.hardware.usb@1.1.vendor \
+    android.hardware.usb@1.2.vendor \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.0.vendor \
+    android.hardware.usb.gadget@1.1.vendor   
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.insmod.sh \
